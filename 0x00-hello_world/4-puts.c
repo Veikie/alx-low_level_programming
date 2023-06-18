f1ce86f18fe1f1abd6c9b5e8543644594e3f1801
Veikie <vicudosen18@gmail.com>
2023-06-18 09:24:39 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints using puts
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
