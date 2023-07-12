0e4eb4c64b3550512ed7128a5fa9ad7609bf2567
Veikie <vicudosen18@gmail.com>
2023-07-12 16:40:15 -0700

    task 0

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,16 +1,18 @@
 #include "main.h"
-/**
- * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
- *
- * Return: dest
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int j;
 
+/**
+* _strncpy - copy a string
+* @dest: input value
+* @src: input value
+* @n: input value
+* Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+
+	int j;
 
 	j = 0;
 	while (j < n && src[j] != '\0')
@@ -23,7 +25,5 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = '\0';
 		j++;
 	}
-
-
 	return (dest);
 }
--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,15 +1,17 @@
 #include "main.h"
-/**
- * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
- *
- * Return: s1[i] - s2[i]
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i;
 
+/**
+* _strcmp - compare string values
+* @s1: input value
+* @s2: input value
+* Return: s1[i] - s2[i]
+*/
+
+int _strcmp(char *s1, char *s2)
+
+{
+
+	int i;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
@@ -18,7 +20,8 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (s1[i] - s2[i]);
 		}
-	i++;
+
+		i++;
 	}
 	return (0);
 }
